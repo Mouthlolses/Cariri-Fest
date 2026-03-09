76a44c7f1555b2df5646ebb4d678c76413025c8a
matheus <140640590+Mouthlolses@users.noreply.github.com>
2026-03-08 22:33:32 -0300

    feed_Iplm

--- composeApp/src/commonMain/kotlin/com/caririfest/app/ui/home/radar/RadarScreen.kt
+++ composeApp/src/commonMain/kotlin/com/caririfest/app/ui/home/radar/RadarScreen.kt
@@ -1,9 +1,77 @@
 package com.caririfest.app.ui.home.radar
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import caririfest.composeapp.generated.resources.Res
+import caririfest.composeapp.generated.resources.caririfestlogo1
+import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
 @Composable
 fun RadarScreen() {
-    Text("RadarScreen")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Image(
+                            painter = painterResource(Res.drawable.caririfestlogo1),
+                            contentDescription = null,
+                            modifier = Modifier.size(72.dp)
+                        )
+
+                        Text(
+                            text = "Feed",
+                            fontSize = 28.sp,
+                            fontWeight = FontWeight.Black,
+                            fontStyle = FontStyle.Italic,
+                            letterSpacing = 1.5.sp,
+                            style = TextStyle(
+                                brush = Brush.verticalGradient(
+                                    listOf(
+                                        Color(0xFFFFEA00),
+                                        Color(0xFFFF9800)
+                                    )
+                                )
+                            )
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF011021)
+                )
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+        ) {
+            Text("RadarScreen")
+        }
+
+    }
 }
